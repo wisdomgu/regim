7509de8265ce453d93818580b6b3295680bcc1ea
Satish Garg <satish.brainiac@gmail.com>
2026-05-01 13:50:27 +0530

    title fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Haha stonks go brr",
+  title: "Regim",
   description: "Market Microstructure & Optimal Execution Research System",
 };
 
